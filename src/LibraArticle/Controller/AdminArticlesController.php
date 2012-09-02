82d5a744407e3b4b20db06763af1e7537509695b
Vitalii Nagara <duke@ejoom.com>
2012-09-02 16:31:14 +0300

    Add remove un/publish actions

--- src/LibraArticle/Controller/AdminArticlesController.php
+++ src/LibraArticle/Controller/AdminArticlesController.php
@@ -5,6 +5,7 @@ namespace LibraArticle\Controller;
 use Zend\View\Model\ViewModel;
 use Zend\Stdlib\RequestInterface as Request;
 use Zend\Stdlib\ResponseInterface as Response;
+use Doctrine\ORM\ORMInvalidArgumentException;
 
 class AdminArticlesController extends AbstractArticleController
 {
@@ -17,9 +18,67 @@ class AdminArticlesController extends AbstractArticleController
 
         return new ViewModel(array(
             'articles' => $articles,
+            'messages' => $this->flashMessenger()->getMessages(),
         ));
     }
+
+    public function update($action = 'unpiblish', $plural = null)
+    {
+        $ids = $this->params()->fromPost('ids', array());
+
+        if (empty($ids)) {
+            $this->flashMessenger()->addMessage('No article was selected');
+            return $this->redirect()->toRoute();
+        }
+
+        foreach ($ids as $id) {
+            try {
+                $article = $this->getRepository()->find($id);
+                switch ($action) {
+                    case 'unpublish':
+                        $article->setState('unpublished');
+                        $message = '%d articles was unpublished successfully';
+                        break;
+
+                    case 'publish':
+                        $article->setState('published');
+                        $message = '%d articles was published successfully';
+                        break;
+
+                    case 'remove':
+                        $message = '%d articles was removed successfully';
+                        $this->getEntityManager()->remove($article);
+                        break;
+
+                    default:
+                        throw new \Exception('can\'t do this action');
+                        break;
+                }
+            } catch (ORMInvalidArgumentException $exc) {
+                $this->flashMessenger()->addMessage('Query error. Wash selected nonexistent article');
+                return $this->redirect()->toRoute();
+            }
+        }
+        $this->getEntityManager()->flush();
+        $this->flashMessenger()->addMessage(sprintf($message, count($ids)));
+        return $this->redirect()->toRoute();
+    }
+
+    public function removeAction()
+    {
+        return $this->update('remove');
+    }
     
+    public function publishAction()
+    {
+        return $this->update('publish');
+    }
+
+    public function unpublishAction()
+    {
+        return $this->update('unpublish');
+    }
+
     public function setClassName($className = 'Article')
     {
         $this->class = $className;
